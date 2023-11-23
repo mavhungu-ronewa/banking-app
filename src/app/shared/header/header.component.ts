4d1dd44d5e010f65ed2a2e0dc3e794c8df067a2f
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2023-11-23 13:21:13 +0200

    adding shared components

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterLinkActive, RouterLink,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  showMobileMenu: boolean = false;
+  public navWidth: any;
+  public navHeight: any;
 
+  constructor(
+
+  ) {}
+
+  ngOnInit() {
+    /*this.navWidth = window.innerWidth;
+    this.navHeight = window.innerHeight;*/
+  }
+
+  //@HostListener('window:resize', ['$event'])
+  /*onWindowResize() {
+    this.navWidth = window.innerWidth;
+    this.navHeight = window.innerHeight;
+  }*/
 }
