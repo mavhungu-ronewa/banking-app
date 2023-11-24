547ca3c8645830977f6059d73c4a45bf758a07c0
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2023-11-24 07:18:01 +0200

    adding checkOut method

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthenticationService } from "../../authentication/services/authentication.service";
 
 @Component({
   selector: 'app-header',
@@ -12,21 +13,33 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
   showMobileMenu: boolean = false;
+  userIsLoggedIn: boolean = false;
   public navWidth: any;
   public navHeight: any;
 
-  constructor(
+  constructor(private authService: AuthenticationService) {}
 
-  ) {}
-
-  ngOnInit() {
+  ngOnInit(): void {
     /*this.navWidth = window.innerWidth;
     this.navHeight = window.innerHeight;*/
+    this.checkLoginStatus();
   }
 
-  //@HostListener('window:resize', ['$event'])
-  /*onWindowResize() {
+  @HostListener('window:resize', ['$event']) onWindowResize() {
     this.navWidth = window.innerWidth;
     this.navHeight = window.innerHeight;
-  }*/
+  }
+
+  // Method to check user login status
+  checkLoginStatus(): void {
+    // Logic to determine user login status and update userIsLoggedIn accordingly
+    this.userIsLoggedIn = this.authService.checkLoginStatus();
+  }
+  checkOut(): void{
+    console.log("Check out button...");
+  }
+  logOut(): void {
+    this.authService.logout();
+    this.checkLoginStatus();
+  }
 }
