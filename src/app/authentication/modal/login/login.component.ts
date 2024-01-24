12a97110ef36a166b2db07a59d29d0ba0de803c7
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2024-01-24 17:46:09 +0200

    adding login modal

--- src/app/authentication/modal/login/login.component.ts
+++ src/app/authentication/modal/login/login.component.ts
@@ -3,38 +3,61 @@ import { CommonModule } from '@angular/common';
 
 
 import { ModalService } from "../../services/modal.service";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
   showModal: boolean = false;
-  constructor(private modalService:ModalService) {}
- /* modalHandler(val: boolean) {
-    const modal: HTMLElement | null = document.getElementById("modal");
-    const button: HTMLElement | null = document.getElementById("button");
-    //var me : HTMLElement|null = document.getElementById("me");
-    if (val) {
-      modal?.classList.remove("hidden");
-      button?.classList.add("hidden");
-    } else {
-      modal?.classList.add("hidden");
-      button?.classList.remove("hidden");
-    }
-  }*/
+  loginError: string ="";
+  constructor(
+    private modalService:ModalService,
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router,
+  ) {}
 
+  loginForm = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    email2: ['', [Validators.required]],
+    expiry: ['', [Validators.required]],
+    cvc: ['', Validators.required],
+  });
   ngOnInit() {
     this.modalService.showModal$.subscribe((value)=> {
       this.showModal = value;
     });
   }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Logging in ...', loginData);
+      this.authService.login(loginData)
+        .subscribe((success)=>{
+            if(success) {
+              this.loginForm.reset();
+              //this.router.navigate(['/home']);
+            }
+            else {
+              this.loginError = '';
+            }
+          }, (error)=> {
+            this.loginError = ''
+          }
+        );
+    }else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
   closeModal() {
-    //this.modalService.closeModal();
     this.modalService.toggleModal();
-    console.log("We are closing modal");
   }
 }
