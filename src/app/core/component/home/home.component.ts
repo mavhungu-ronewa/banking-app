42430bf90397307481e9a917d86c0e36fd16a981
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2024-03-06 01:37:39 +0200

    adding function #addToCart

--- src/app/core/component/home/home.component.ts
+++ src/app/core/component/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from  "@ngrx/store";
-import { Observable, of } from "rxjs";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { FlightService } from "../../../services/flight.service";
 import * as ProductActions from "../../../store/product.actions";
 import { Products } from "../../../modules/products";
 import { selectProducts } from "../../../store/product.selectors";
+import * as CartActions from '../../../store/cart/cart.actions';
+import { ProductService } from "../../../services/product.service";
+import { CartService } from "../../../services/cart.service";
 
 @Component({
   selector: 'app-home',
@@ -18,8 +20,8 @@ import { selectProducts } from "../../../store/product.selectors";
 })
 export class HomeComponent implements OnInit {
 
-  Flights : Products[] = [];
-  Products: Products[] = [];
+  //Flights : Products[] = [];
+  //Products: Products[] = [];
   //products$: Observable<Products[]>;
   Hotels: string[] = [];
   Rides: string[] = [];
@@ -31,7 +33,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private store: Store,
     private flights: FlightService,
-    private productService: FlightService
+    private productService: ProductService,
+    private cartService: CartService
   ) {
     //this.count$ = store.select('count');
   }
@@ -45,20 +48,20 @@ export class HomeComponent implements OnInit {
     //this.getProducts();
   }
 
-  getFlights() {
+ /* getFlights() {
     this.flights.getFlights().subscribe({
       next: (result)=>{
         this.Flights = result;
       }
     });
-  }
-  getProducts() {
+  }*/
+  /*getProducts() {
     this.flights.getProducts().subscribe({
       next: (products)=> {
         this.Products = products;
       }
     })
-  }
+  }*/
   getProduct() {
     this.store.dispatch(ProductActions.getProducts());
   }
@@ -70,6 +73,10 @@ export class HomeComponent implements OnInit {
   }
   getRides() {
     this.Rides = [];
+  }
+  addToCart(product: Products) {
+    //this.store.dispatch(CartActions.addToCart({ product }));
+    this.cartService.addToCart(product);
   }
   addProduct() {
     const newProduct = { id: '1', name: 'New Product'}; /* add product using real data */
