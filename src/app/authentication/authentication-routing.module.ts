4f223878679e936f38c33aeb501fc5ba3cd031f5
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2024-03-19 13:24:27 +0200

    adding user authentication route

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
+import { RegistrationComponent } from "./modal/registration/registration.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  {
+    path: 'user',
+    children: [
+      { path: 'register', component: RegistrationComponent },
+    ]
+  }
 ];
 
 @NgModule({
